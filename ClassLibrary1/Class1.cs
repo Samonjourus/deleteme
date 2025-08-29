344ef7d07f7f32fa2549e7676ba448f80cf1ee02
Benjamin Standfield <benjaminstandfield@gmail.com>
2025-08-29 16:45:20 -0400

    initial commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,23 @@
+﻿namespace ClassLibrary1
+{
+    public class Cube
+    {
+        double length;
+        public static double getSideCount() { return 4; }
+        public double getArea() { return 5; }
+        public double getPerimiter() { return 10; }
+    }
+
+    public class whocares
+    {
+        public double getSideCount()
+        {
+            Cube x = new Cube();
+            x.getArea();
+            Cube.getSideCount();
+
+            return 0;
+        }
+    }
+}
+
